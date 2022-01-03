a733341b62c0047e6744cc00862c5bfd2a13e801
Alexander Brix <alexander.brix@ericsson.com>
2022-01-03 17:15:37 +0100

    test2

--- moviedb/src/components/Thumb/index.js
+++ moviedb/src/components/Thumb/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 //styles
 import { Image } from './Thumb.styles';
-
+//test2
 const Thumb = ({ image, movieId, clickable }) => (
     <div>
         <Image src={image} alt='movie-thumb' />
