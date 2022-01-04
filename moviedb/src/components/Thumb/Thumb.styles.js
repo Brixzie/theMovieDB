0efcb8a1449a2a6ee3b9524081b0eb32748b66df
Alexander Brix <alexander.brix@ericsson.com>
2022-01-04 16:09:47 +0100

    thumbnails added

--- moviedb/src/components/Thumb/Thumb.styles.js
+++ moviedb/src/components/Thumb/Thumb.styles.js
@@ -1,3 +1,22 @@
 import styled from "styled-components";
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  width: 100%;
+  max-width: 720px;
+  transition: all 0.3s;
+  object-fit: cover;
+  border-radius: 20px;
+  // border-radius: 50px;
+  animation: animateThumb 3.5s;
+
+  @keyframes animateThumb{
+    from {
+      opacity: 0;
+      // border-radius: 0px;
+    }
+    to {
+      opacity: 1;
+      // border-radius: 50px;
+    }
+  }
+`;
