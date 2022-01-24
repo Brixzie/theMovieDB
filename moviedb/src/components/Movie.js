7fea7d77e1078289391e4f5c3c27dc4570ddb04a
Alexander <brixzen@hotmail.com>
2022-01-24 15:34:35 +0100

    Actor Images

--- moviedb/src/components/Movie.js
+++ moviedb/src/components/Movie.js
@@ -10,12 +10,14 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 //Hook
 import { useMovieFetch } from "./hooks/useMovieFetch";
 
 //Image
 import NoImage from '../images/no_image.jpg';
+import { act } from "react-dom/cjs/react-dom-test-utils.production.min";
 
 
 const Movie = () => {
@@ -35,6 +37,19 @@ const Movie = () => {
           budget={movie.budget} 
           revenue={movie.revenue} 
         />
+        <Grid header='Actors'>
+          {movie.actors.map(actor => 
+            <Actor
+              key={actor.credit_id}
+              name={actor.name}
+              character={actor.character}
+              imageUrl={
+                actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+              }
+              />
+            )}
+        </Grid>
       </div>
     </>
 
